a974d12630a919b6c44e1f9f9e85b6a2b7767d3e
Jonfras <jonas.kreuzh@gmail.com>
2023-05-22 18:27:02 +0200

    anfang neicheste übung

--- app/src/main/java/net/htlgkr/krejo/toDoList/login/LoginActivity.java
+++ app/src/main/java/net/htlgkr/krejo/toDoList/login/LoginActivity.java
@@ -1,7 +1,23 @@
 package net.htlgkr.krejo.toDoList.login;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import net.htlgkr.krejo.toDoList.R;
 
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_login);
+
+
+    }
+
+    private void startNewActivity(){
+        Intent intent = new Intent();
+        startActivity(intent);
+    }
 }
